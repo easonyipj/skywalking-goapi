1be926e93da6be9acd8e660d9d7aee69b1924da6
easonyipj <yipingjian@qq.com>
2022-07-14 00:57:05 +0800

    [feat]add golang runtime metrics api

--- dependencies.sh
+++ dependencies.sh
@@ -17,7 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export COLLECT_PROTOCOL_SHA=112368d0af96d0bb083d8b59a424f5e51c1bfbc6
+export COLLECT_PROTOCOL_SHA=6d9e29fc7003daacb750eed25dcd594aa94b2ff0
 export QUERY_PROTOCOL_SHA=3adb9f68649b1a03fa3fa664192ff4bd7523f18b
 export ENVOY_SERVICE_PROTOCOL_SHA=533b32f1b390a3a88ec2008d0561e07c926d879a
 export XDS_SERVICE_PROTOCOL_SHA=25de7278fc844d392d607214f36dbedf50f167ee
--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.17.12
-	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/vektah/gqlparser/v2 v2.4.6
-	google.golang.org/genproto v0.0.0-20210624195500-8bfb893ecb84 // indirect
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.28.0
+	skywalking.apache.org/repo/goapi v0.0.0-20220713141736-9874f2f4f8cc
 )
