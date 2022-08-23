9e3ff637bb20a3d3bd8cb9d3710c3e0d3f4364e6
easonyipj <yipingjian@qq.com>
2022-08-23 16:20:24 +0800

    [feat]add golang runtime metrics api

--- dependencies.sh
+++ dependencies.sh
@@ -17,7 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export COLLECT_PROTOCOL_SHA=6e928e8a759abb4bfd3f3729d13dc68e8e00656a
+export COLLECT_PROTOCOL_SHA=fa70b87c9b50a4ba7f78ff9599c70279752b32eb
 export QUERY_PROTOCOL_SHA=3adb9f68649b1a03fa3fa664192ff4bd7523f18b
 export ENVOY_SERVICE_PROTOCOL_SHA=533b32f1b390a3a88ec2008d0561e07c926d879a
 export XDS_SERVICE_PROTOCOL_SHA=25de7278fc844d392d607214f36dbedf50f167ee
